a33ed2c522e00aff9ce11a78901b1987835baedb
JonasFiechter <jonasrfiechter@gmail.com>
2022-02-27 21:14:17 -0300

    magic challenge done

--- magic.py
+++ magic.py
@@ -10,26 +10,39 @@ magic.list_slice(list, tuple) is a function that returns the items in the list t
 specified indexes. If the length of the new list is 0, return -1.
 '''
 
+
 class Magic:
     def __init__(self):
         pass
 
-    def replace_letter(string, character, new_character):
+    def replace_letter(string, character, new_character):  # Traditional method to replace
         new_string = ''
         for c in string:
             if c == character:
                 new_string += new_character
             else:
                 new_string += c
-        
         return new_string
 
-    def replace_comprehension(string, character, new_character):
-        
+    def replace_comprehension(string, character, new_character):  # Alternative method to replace 
         return ''.join(c.replace(character, new_character) for c in string)
+
+    def str_length(string):        
+        return len(string)
+
+    def list_slice(string:str, tuple:tuple):
+        if len(string) == 0:
+            return -1
+        return string[tuple[0]:tuple[1]]
+
+    def trim(string):
+        return ' ' + string + ' '
 
 
 if __name__ == '__main__':
     var = 'Beyond files and things'
     print(Magic.replace_letter(var, 'a', '#'))
     print(Magic.replace_comprehension(var, 'a', '@'))
+    print(Magic.str_length(f'length of string: {var}'))
+    print(Magic.list_slice(var,(2, 4)))
+    print(Magic.trim(var))
