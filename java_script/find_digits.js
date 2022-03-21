09f3b228ee02c27eeb28dd9b119624a8c12b9372
JonasFiechter <jonasrfiechter@gmail.com>
2022-03-21 19:22:19 -0300

    update

---
+++ java_script/find_digits.js
@@ -0,0 +1,15 @@
+/*
+Find Number of Digits in Number
+
+Create a function that will return an integer number corresponding 
+to the amount of digits in the given integer num.
+*/
+
+function find_digits(number_) {
+    if (typeof(number_) !== 'number') {
+        return -1
+    }
+    return String(number_).length
+}
+
+console.log(find_digits('32'))
