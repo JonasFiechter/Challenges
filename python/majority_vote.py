7fd8da8f5bc0f92741061833c3b442b5338b4933
JonasFiechter <jonasrfiechter@gmail.com>
2022-07-29 22:14:53 -0300

    .
    .

--- python/majority_vote.py
+++ python/majority_vote.py
@@ -15,7 +15,11 @@ If there is no majority element, return None.
 If the list is empty, return None.
 '''
 
-#  Solution 1
+#  Solution 1 (it doesn't solve the equal situations)
+from numpy import equal
+from pandas import options
+
+
 def majority_vote(list_):
     list_of_options = []
     count_of_options = []
@@ -28,9 +32,30 @@ def majority_vote(list_):
     #   Counting each option
     for i in list_of_options:
         count_of_options.append(list_.count(i))
+
+    #   Selecting the bigger
+    bigger_index = 0
+    for n, i in enumerate(count_of_options):
+        if i > bigger_index:
+            bigger_index = n
     
-    print(count_of_options)
+    return list_[bigger_index]
+
+
+#   Solution 2 (dicts usually makes things easier)
+def majority_vote(list_):
+    options_dict = {key: list_.count(key) for key in list_}
+
+    equal = list(options_dict.values())[0]
+    #  Check for equality
+
+    
+    for key, value in options_dict.items():
+        if value == max(options_dict.values()):
+            return key
+    return options_dict
 
 
 
-majority_vote(["A", "A", "A", "B", "C", "A"])
+print(majority_vote(["A", "A", "A", "B", "C", "A"]))
+print(majority_vote(["A", "B", "B", "A", "C", "C"]))
