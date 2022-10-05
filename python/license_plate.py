ddfddd9aa0cf18d58b3427a0260f55e79d096a27
JonasFiechter <jonasrfiechter@gmail.com>
2022-10-04 21:13:15 -0300

    update1

--- python/license_plate.py
+++ python/license_plate.py
@@ -7,7 +7,7 @@ regulations, attach it back and proceed with the journey.
 
 Create a solution that can format the dmv number into a plate number with 
 correct grouping. The function input consists of a string s and group length n. 
-The output has to be upper case characters and digits. The new groups are made 
+The output has to be upper case characters and digits. The new count_groups are made 
 from right to left and connected by -. The original order of the dmv number is 
 preserved.
 
@@ -22,15 +22,43 @@ license_plate("nlj-206-fv", 3) ➞ "NL-J20-6FV"
 '''
 
 def license_plate(s, n):
-    groups = len(s.replace('-', '')) // n
-    group_count = 0
-    new_string = ''
+    count_groups = len(s.replace('-', '')) // n
+    groups = [[] for i in range(count_groups)]
+    cursor = 1
     
-    print(s[-1:(n+1) * (-1):-1])
+    for group in range(count_groups):
+        while True:
+            if cursor <= n:
+                if s[-1] != '-':
+                    groups[group].append(s[-1])
+                    s = s.strip(s[-1])
+                    cursor += 1
+                else:
+                    s = s.strip(s[-1])
+            else:
+                cursor = 1
+                break
+    if not s:
+        for group in groups[::-1]:
+            for char in group[::-1]:
+                s += char.upper()
+            s += '-'
     
-    for group in range(groups):
-        print()
+    elif s[-1] == '-':
+        for group in groups[::-1]:
+            for char in group[::-1]:
+                s += char.upper()
+            s += '-'
+    
+    else:
+        s += '-'
+        for group in groups[::-1]:
+            for char in group[::-1]:
+                s += char.upper()
+            s += '-'
 
-        return 
+    return s[:-1]
 
-license_plate("5F3Z-2e-9-w", 4)
+
+print(license_plate("5F3Z-2e-9-w", 4))
+print(license_plate("2-5g-3-J", 2))
